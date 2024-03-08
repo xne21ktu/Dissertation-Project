d5e9c587841d9458573c99393f10af3a3f233ab2
James Massey <xne21ktu@uea.ac.uk>
2024-03-08 13:45:00 +0000

    applying filtered data to tidying code

--- scripts/eyespots.R
+++ scripts/eyespots.R
@@ -27,32 +27,33 @@ eyespots_filtered <- read_csv("data/Experiment 1 edited.csv")
 
 eyespots # call the dataframe
 
+eyespots_filtered
+
 str(eyespots) # check structure of data
 
+str(eyespots_filtered)
 
 #_________________________----
 # TIDY DATA ----
 
 # check data is in a tidy format
-head(eyespots)
+head(eyespots_filtered)
 
 # check variable names
-colnames(eyespots)
+colnames(eyespots_filtered)
 
 # check for duplication
-eyespots %>% 
+eyespots_filtered %>% 
   duplicated() %>% 
   sum()
 
 
-eyespots$collection <- as_factor(eyespots$collection)
-eyespots$design <- as_factor(eyespots$design)
-eyespots$predated <- as_factor(eyespots$predated)
-eyespots$location <- as_factor(eyespots$location)
-eyespots$rain <- as_factor(eyespots$rain)
-eyespots$overcast <- as_factor(eyespots$overcast)
-eyespots$sun <- as_factor(eyespots$sun)
-eyespots$slug <- as_factor(eyespots$slug)
+eyespots_filtered$collection <- as_factor(eyespots_filtered$collection)
+eyespots_filtered$design <- as_factor(eyespots_filtered$design)
+eyespots_filtered$predated <- as_factor(eyespots_filtered$predated)
+eyespots_filtered$location <- as_factor(eyespots_filtered$location)
+eyespots_filtered$weather <- as_factor(eyespots_filtered$weather)
+eyespots_filtered$slug <- as_factor(eyespots_filtered$slug)
 # converts selected columns into a factor so they can be used in analyses
 
 summary(eyespots)
@@ -60,48 +61,46 @@ summary(eyespots)
 # quick summary
 
 # missing values
-eyespots %>%
+eyespots_filtered %>%
   is.na() %>% 
   sum()
-# 29 NA values, which represent missing dummies from the experiment
+# original (eyespots) contained 29 NA values, which represent missing dummies from the experiment and were removed
+# so now NA numbers = 0
 
-eyespots %>% 
+eyespots_filtered %>% 
   group_by(design, predated) %>% 
   summarise(n = n()) %>%
   mutate(prob_obs = n/sum(n))
 
 # can see that design 1 was the most predated, and design 3 was predated the least
 # but only differed from design 2 by 12.
-# Design 1 had 80% chance of predation, 2 had a 57% chance and 3 had a 54% chance
+# Design 1 had 81% chance of predation, 2 had a 59% chance and 3 had a 55% chance
 # Can see number of missing dummies for each design
 
-eyespots %>% 
+eyespots_filtered %>% 
   group_by(location, predated) %>% 
   summarise(n = n()) %>%
   mutate(prob_obs = n/sum(n))
 
-#Can see that location 2 (69%) had a higher level of predation than location 1 (59%)
+#Can see that location 2 had a higher level of predation (72%) than location 1 (60%)
 
 
 #_________________________----
 # PLOTS ----
 
-eyespots %>% 
+eyespots_filtered %>% 
   ggplot(aes(x=design, fill=predated))+
   geom_bar(position=position_dodge())+
   coord_flip()
 
-eyespots %>% 
+eyespots_filtered %>% 
   ggplot(aes(x=design,y=predated, fill=predated))+
   geom_bar(position="stack", stat="identity")+
   coord_flip()
-# stacked bar plot shows that most NA's are in design 2, NA's are lowest in design 1
-# which could suggest this is why it has the lowest survival rate but the difference in NA's
-# is not large enough.
-# However design 1 shows a higher overall count which may be playing a role in differences between designs
+# Can see a larger predation rate and sample size for design 1, not much difference
+# between design 2 and 3
 
-
-eyespots %>% 
+eyespots_filtered %>% 
   ggplot(aes(x=location,y=predated, fill=predated))+
   geom_bar(position="stack", stat="identity")+
   coord_flip()
